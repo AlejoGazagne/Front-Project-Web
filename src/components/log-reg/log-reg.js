12bd722c06458b8320c6aa339e3b568a7c9dbcfb
Alejo Gazagne <gazagnealejo@gmail.com>
2024-05-11 21:57:46 -0300

    login and register

---
+++ src/components/log-reg/log-reg.js
@@ -0,0 +1,96 @@
+const btnSesion = document.getElementById("iniciar-sesion");
+const btnIrOlvContrasenia = document.getElementById("olvide-contrasenia");
+const btnIrRegistrarse = document.getElementById("registrarme");
+
+const mail = document.getElementById("gmail");
+const password = document.getElementById("contrasenia");
+
+const iniciarSesion = document.getElementById("init-sesion");
+
+const recuperarContrasenia = document.getElementById("recuperar-contrasenia");
+const continuarRecuperacion = document.getElementById("continuar-rec");
+
+const registrarse = document.getElementById("regist");
+const btnRegistrar = document.getElementById("probando");
+
+const seleccionCuenta = document.getElementById("seleccion-cuenta");
+const formVisitante = document.getElementById("formVisitante");
+const emailUsuario = document.getElementById("regGmail");
+const contraseniaUsuario = document.getElementById("regContrasenia");
+
+const formPublicador = document.getElementById("formPublicador");
+const nombrePublicador = document.getElementById("pubNombre");
+const numeroPublicador = document.getElementById("pubNumero");
+const emailPublicador = document.getElementById("pubGmail");
+const contraseniaPublicador = document.getElementById("pubContrasenia");
+
+const error = document.getElementById("error");
+
+btnSesion.addEventListener('click', () => {
+    if(mail.value != null && password.value != null){
+        console.log('click');
+        console.log(mail.value);
+        console.log(password.value)
+        // fetch('http://localhost:3000/login',{
+        //     method: "POST",
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({
+        //         email: mail.value,
+        //         password: password.value
+        //     })
+        // }).then(async response  => {
+        //     const rsp = await response.json();
+        //     localStorage.setItem('token', rsp.token);
+        //     localStorage.setItem('user', rsp.user);
+        //     modalRegistro.classList.remove('mostrar');
+        // })
+    }
+});
+
+btnIrOlvContrasenia.addEventListener('click', () => {
+    recuperarContrasenia.style.display = 'flex';
+    iniciarSesion.style.display = 'none';
+    registrarse.style.display = 'none'; 
+});
+
+continuarRecuperacion.addEventListener('click', () => {
+    recuperarContrasenia.style.display = 'none';
+    iniciarSesion.style.display = 'flex';
+});
+
+btnIrRegistrarse.addEventListener('click', () => {
+    registrarse.style.display = 'flex';
+    iniciarSesion.style.display = 'none';
+    recuperarContrasenia.style.display = 'none';
+});
+
+seleccionCuenta.addEventListener('click', () => {
+    if (seleccionCuenta.value == 1){
+        formVisitante.style.display = 'flex';
+        formPublicador.style.display = 'none';
+        
+    }
+    if (seleccionCuenta.value == 2){
+        formVisitante.style.display = 'none';
+        formPublicador.style.display = 'flex';
+    }
+});
+
+btnRegistrar.addEventListener('click', () => {
+    event.preventDefault();
+    if ((emailUsuario.value == '' || contraseniaUsuario.value == '') && seleccionCuenta.value == 1){
+        error.classList.add('mostrar');
+        console.log('error');
+    }
+    if ((nombrePublicador.value == '' || numeroPublicador.value == '' || emailPublicador.value == '' || contraseniaPublicador.value == '') && seleccionCuenta.value == 2){
+        error.classList.add('mostrar');
+        console.log('error');
+    }
+    if (seleccionCuenta.value != 1 && seleccionCuenta.value != 2){
+        error.classList.add('mostrar');
+        console.log('error');
+    }
+
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,51 @@
 const navbar = document.getElementById("navbar");
-const modalRegistro = document.getElementById("modal-registro");
-const inputGmail = document.getElementById("gmail");
-const inputContrasenia = document.getElementById("contrasenia");
+const log_reg = document.getElementById("log-reg");
+
+const marcoFlotante = document.getElementById("marco-flotante");
+
+const inputGmail = log_reg.contentDocument.getElementById("gmail");
+const inputContrasenia = log_reg.contentDocument.getElementById("contrasenia");
+const atras = document.getElementById("atras");
 
 navbar.addEventListener('load', () => {
-  btnRegistro = navbar.contentDocument.getElementById("btn-registro");
+  btnSesion = navbar.contentDocument.getElementById("btn-sesion");
   
-  btnRegistro.addEventListener('click', () => {
-    btnContinuar = document.getElementById("continuar");
+  btnSesion.addEventListener('click', () => {
+    
+    log_reg.classList.add('mostrar');
+    atras.classList.add('mostrar');
+    marcoFlotante.classList.add('mostrar');
 
-    modalRegistro.classList.add('mostrar');
+    // log_reg.addEventListener('load', () => {
+    //   btnContinuar = log_reg.contentDocument.getElementById("continuar");
 
-    btnContinuar.addEventListener('click', () => {
-      if(inputGmail.value != null && inputContrasenia.value != null){
-      console.log("entro?")
-      fetch('http://localhost:3000/login',{
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email: inputGmail.value,
-          password: inputContrasenia.value
-        })
-      }).then(async response  => {
-        const rsp = await response.json();
-        localStorage.setItem('token', rsp.token);
-        localStorage.setItem('user', rsp.user);
-        modalRegistro.classList.remove('mostrar');
-      })
+    //   btnIniciarSesion.addEventListener('click', () => {
+    //     console.log('click');
+    //     // if(inputGmail.value != null && inputContrasenia.value != null){
+    //     // fetch('http://localhost:3000/login',{
+    //     //   method: "POST",
+    //     //   headers: {
+    //     //     'Content-Type': 'application/json'
+    //     //   },
+    //     //   body: JSON.stringify({
+    //     //     email: inputGmail.value,
+    //     //     password: inputContrasenia.value
+    //     //   })
+    //     // }).then(async response  => {
+    //     //   const rsp = await response.json();
+    //     //   localStorage.setItem('token', rsp.token);
+    //     //   localStorage.setItem('user', rsp.user);
+    //     //   modalRegistro.classList.remove('mostrar');
+    //     // })
 
-      }
-    });
+    //     // }
+      // });
+    });  
 
   });
 
+atras.addEventListener('click', () => { 
+  log_reg.classList.remove('mostrar');
+  atras.classList.remove('mostrar');
+  marcoFlotante.classList.remove('mostrar');
 });
