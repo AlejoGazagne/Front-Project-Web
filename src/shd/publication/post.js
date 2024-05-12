d24d698ec5374a0a9aa936db5a90bdb31a3c71d6
Pandulc <gabiguibe@gmail.com>
2024-05-12 16:16:59 -0300

    post structure

--- src/shd/publication/post.js
+++ src/shd/publication/post.js
@@ -2,3 +2,7 @@ let map = L.map("map", {
   center: [-31.433519266013796, -64.27658547423133],
   zoom: 13,
 });
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+}).addTo(map);
