35753cb5696b29830f6e85be8e385305d0286238
Pandulc <gabiguibe@gmail.com>
2024-05-12 10:05:26 -0300

    some changes 2

---
+++ src/shd/catalogue/catalogue.js
@@ -0,0 +1,23 @@
+let navbar = document.getElementById("navbar");
+
+let type = document.getElementById("type");
+let price = document.getElementById("priceInput");
+
+let btnDRoom = document.getElementById("decrementRoom");
+let roomCount = document.getElementById("bedroomCount");
+let btnIRoom = document.getElementById("incrementRoom");
+
+let btnDBathroom = document.getElementById("decrementBathroom");
+let bathroomCount = document.getElementById("bathroomCount");
+let btnIBathroom = document.getElementById("incrementBathroom");
+
+let btnDGarage = document.getElementById("decrementGarage");
+let garageCount = document.getElementById("garageCount");
+let btnIGarage = document.getElementById("incrementGarage");
+
+let pool = document.getElementById("poolCheck");
+let pets = document.getElementById("petsCheck");
+
+let search = document.getElementById("btnSearch");
+
+navbar.addEventListener("load", () => {});
